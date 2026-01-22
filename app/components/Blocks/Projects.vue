c76dbfead7f3513b11f16963e65ba8c52939dcce
neharepos <nehakeshrimail@gmail.com>
2026-01-22 16:55:58 +0530

    fixed the button

--- app/components/Blocks/Projects.vue
+++ app/components/Blocks/Projects.vue
@@ -45,13 +45,17 @@ const projectData = await queryCollection('project').order('duration', 'DESC').l
          </button>
       </div> -->
 
-      <div class="mt-10 flex justify-start sm:justify-start">
-   <NuxtLink 
-     to="/project" 
-     class="border px-6 ml-16 py-2 mb-8 border-zinc-700 text-zinc-400 hover:text-white hover:border-zinc-500 rounded-xl transition-all cursor-pointer inline-block"
-   >
-     View all projects
-   </NuxtLink>
+    <div class="mx-auto max-w-6xl px-4 sm:px-8">
+  
+  <div class="mt-8 flex justify-start">
+    <NuxtLink 
+      to="/project" 
+      class="border px-6 ml-5 py-2 mb-8 border-zinc-700 text-zinc-400 hover:text-white hover:border-zinc-500 rounded-xl transition-all cursor-pointer inline-block"
+    >
+      View all projects
+    </NuxtLink>
+  </div>
+
 </div>
     
     
