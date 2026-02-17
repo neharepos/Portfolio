d7e1c36d8a89906c3ebd1191699b361ecbabe92e
NishaantKrSingh <nishaantkrsingh@gmail.com>
2026-02-17 13:37:35 +0530

    reverted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-12-16',
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-
-  
 
   css: [
     // KaTeX styles (needed to display math correctly)
@@ -17,14 +15,8 @@ export default defineNuxtConfig({
   ],
   modules: ['@nuxt/content', '@nuxtjs/color-mode'],
   content: {
-    database: {
-      type: 'sqlite',
-      // This forces it to stay within the bundled application code 
-      // rather than drifting into Vercel's /tmp directory
-      filename: './content.db' 
-    },
     build: {
-
+      
       markdown: {
         // Add remark plugin to parse $...$ and $$...$$ math syntax
         remarkPlugins: {
@@ -41,23 +33,3 @@ export default defineNuxtConfig({
     }
   }
 })
-function defineNuxtConfig(arg0: {
-  compatibilityDate: string; vite: { plugins: any[]; }; css: string[]; modules: string[]; content: {
-    database: { type: string; }; build: {
-      markdown: {
-        // Add remark plugin to parse $...$ and $$...$$ math syntax
-        remarkPlugins: { 'remark-math': {}; };
-        // rehype plugin to render math to HTML using KaTeX
-        rehypePlugins: {
-          'rehype-katex': {
-            // optional options; output: 'html' is common (default)
-            output: string;
-          };
-        };
-      };
-    };
-  };
-}) {
-  throw new Error("Function not implemented.");
-}
-
